48eca9581d2c418cd49a68d1bd6cfe61c826e28a
Akash Kumar <akashyadav112314@gmail.com>
2021-12-23 15:02:07 +0530

    changed to original code with the function

--- string_increment_1.cpp
+++ string_increment_1.cpp
@@ -3,47 +3,40 @@
 using std::cout;
 using std::string;
 //increments each alphabet of a word by its next alphabet
-// string increment_by_one(string word) {
-//     char ch;
-//     string new_word = "";
-//     for (int i = 0; i < word.length(); i++) {
-//         ch = char(word[i] + 1);
-//         new_word.append(1, ch);
-//     }
-//     return new_word;
-// }
+string increment_by_one(string word) {
+    char ch;
+    string new_word = "";
+    for (int i = 0; i < word.length(); i++) {
+        ch = char(word[i] + 1);
+        new_word.append(1, ch);
+    }
+    return new_word;
+}
 
 int main() {
-    string str;  //, word = "";
-    char ch;
-    int i = 0, count = 1, p, q;
+    string str, word = "";
+    int i = 0, count = 0, p, q;
     cout << "Enter a string: ";
     getline(std::cin, str);
     cout << "Enter word numbers: ";
     std::cin >> p >> q;
     while (i < str.length()) {
-        if (str[i] == ' ') {
-            cout << str[i];
+        if (str[i] == ' ')
             i++;
-        } else {
+        else {
             while (str[i] != ' ') {
-                //    word.append(1, str[i]);
-                if (count == p || count == q) {
-                    ch = char(str[i] + 1);
-                    cout << ch;
-                } else
-                    cout << str[i];
+                word.append(1, str[i]);
                 i++;
                 if (i == str.length())
                     break;
             }
             count += 1;
-            //if (count == p || count == q) {
-            //    word = increment_by_one(word);
+            if (count == p || count == q) {
+                word = increment_by_one(word);
+            }
+            cout << word << " ";
+            word = "";
         }
-        //cout << word << " ";
-        //word = "";
     }
     return 0;
 }
-//}
