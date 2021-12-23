95974d1d6e78044e39e489fd58d38875ad8fe245
Akash Kumar <akashyadav112314@gmail.com>
2021-12-23 15:08:16 +0530

    better way

--- string_increment_1.cpp
+++ string_increment_1.cpp
@@ -2,40 +2,30 @@
 #include <string>
 using std::cout;
 using std::string;
-//increments each alphabet of a word by its next alphabet
-string increment_by_one(string word) {
-    char ch;
-    string new_word = "";
-    for (int i = 0; i < word.length(); i++) {
-        ch = char(word[i] + 1);
-        new_word.append(1, ch);
-    }
-    return new_word;
-}
 
 int main() {
-    string str, word = "";
-    int i = 0, count = 0, p, q;
+    string str;
+    int i = 0, count = 1, p, q;
     cout << "Enter a string: ";
     getline(std::cin, str);
     cout << "Enter word numbers: ";
     std::cin >> p >> q;
     while (i < str.length()) {
-        if (str[i] == ' ')
+        if (str[i] == ' ') {
+            cout << str[i];
             i++;
-        else {
+        } else {
             while (str[i] != ' ') {
-                word.append(1, str[i]);
+                if (count == p || count == q)
+                    cout << char(str[i] + 1);
+                else
+                    cout << str[i];
                 i++;
+
                 if (i == str.length())
                     break;
             }
             count += 1;
-            if (count == p || count == q) {
-                word = increment_by_one(word);
-            }
-            cout << word << " ";
-            word = "";
         }
     }
     return 0;
