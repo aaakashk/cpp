4364b7b146d4d915411f4d7ee2686c243406bf4a
Akash Kumar <akashyadav112314@gmail.com>
2021-12-27 21:43:19 +0530

    question asked in practical

---
+++ Practical_questions/operator_overloading_complex.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class Complex {
+    int real, imag;
+
+   public:
+    Complex(int x = 0, int y = 0) {
+        real = x;
+        imag = y;
+    }
+    Complex operator*(Complex B) {
+        Complex A;
+        A.real = this->real * B.real;
+        A.imag = this->imag * B.imag;
+        return A;
+    }
+
+    Complex operator*(int z) {
+        Complex A;
+        A.real = this->real * z;
+        A.imag = this->imag * z;
+        return A;
+    }
+
+    Complex operator+(Complex B) {
+        Complex A;
+        A.real = this->real + B.real;
+        A.imag = this->imag + B.imag;
+        return A;
+    }
+
+    Complex operator+(int z) {
+        Complex A;
+        A.real = this->real + z;
+        A.imag = this->imag + z;
+        return A;
+    }
+
+    Complex operator-(Complex B) {
+        Complex A;
+        A.real = this->real - B.real;
+        A.imag = this->imag - B.imag;
+        return A;
+    }
+    //friend function is needed when a non-object calls an operator
+    friend Complex operator*(int z, Complex B);
+
+    void display() {
+        cout << real << " + i(" << imag << ")" << endl;
+    }
+};
+
+Complex operator*(int z, Complex B) {
+    Complex A;
+    A.real = z * B.real;
+    A.imag = z * B.imag;
+    return A;
+}
+
+int main() {
+    Complex A, B(4, -2), C(5, 3), D(7, 2), E(6, 8);
+    cout << "B = ";
+    B.display();
+    cout << "C = ";
+    C.display();
+    cout << "D = ";
+    D.display();
+    cout << "E = ";
+    E.display();
+    A = B * C;
+    A.display();
+    A = B * 8;
+    A.display();
+    A = 8 * B;
+    A.display();
+    A = B + C + D - 5 * E + 8;
+    A.display();
+}
