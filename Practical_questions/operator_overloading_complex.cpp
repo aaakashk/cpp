a4c63323b5c0cfb0aa3a2ed4b666ee4c894b6ae1
Akash Kumar <akashyadav112314@gmail.com>
2021-12-28 23:24:47 +0530

    added a new object

--- Practical_questions/operator_overloading_complex.cpp
+++ Practical_questions/operator_overloading_complex.cpp
@@ -60,7 +60,8 @@ Complex operator*(int z, Complex B) {
 }
 
 int main() {
-    Complex A, B(4, -2), C(5, 3), D(7, 2), E(6, 8);
+    Complex A(1, 1), B(4, -2), C(5, 3), D(7, 2), E(6, 8), F(7, 2);
+    A.display();
     cout << "B = ";
     B.display();
     cout << "C = ";
@@ -71,10 +72,12 @@ int main() {
     E.display();
     A = B * C;
     A.display();
-    A = B * 8;
+    A = A * 8;
     A.display();
-    A = 8 * B;
+    A = 8 * A;
     A.display();
     A = B + C + D - 5 * E + 8;
     A.display();
+    F = F * 8;
+    F.display();
 }
