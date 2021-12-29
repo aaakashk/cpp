8ece4191d200719221cba7d4d999e5a565055f0d
Akash Kumar <akashyadav112314@gmail.com>
2021-12-29 19:47:59 +0530

    added question 3

--- Lab_Manual_3rd_sem/q3_class_BankAccount.cpp
+++ Lab_Manual_3rd_sem/q3_class_BankAccount.cpp
@@ -3,12 +3,51 @@ using std::cin;
 using std::cout;
 
 class BankAccount {
-    std::string depositor_name;
-    int account_number;
+    std::string depositorName;
+    int accountNumber;
     int balance;
+    std::string accountType;
 
    public:
     void get() {
-        cout << "";
+        balance = 0;
+        cout << "Enter the depositor's name: ";
+        getline(cin, depositorName);
+        cout << "Enter the depositor's account number: ";
+        cin >> accountNumber;
+        cout << "Enter the depositor's account type: ";
+        getline(cin.ignore(), accountType);
+    }
+
+    void deposit() {
+        int depositAmount;
+        cout << "Enter the amount to deposit: ";
+        cin >> depositAmount;
+        balance += depositAmount;
+        cout << "Successfully deposited amount: " << balance << std::endl;
+    }
+
+    void withdraw() {
+        int withdrawAmount;
+        cout << "Enter the amount to withdraw: ";
+        cin >> withdrawAmount;
+        if (withdrawAmount < balance) {
+            balance -= withdrawAmount;
+        }
+        cout << "Successfully withdrawn amount: " << withdrawAmount << std::endl;
+    }
+
+    void displayNameAndBalance() {
+        cout << "-------OUTPUT-------" << std::endl;
+        cout << "Name of the depositor is " << depositorName << std::endl;
+        cout << "Remaining balance: " << balance << std::endl;
     }
 };
+
+int main() {
+    BankAccount acc;
+    acc.get();
+    acc.deposit();
+    acc.withdraw();
+    acc.displayNameAndBalance();
+}
