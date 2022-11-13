f1f15054ad65a1c8fad05275fe0b12b48fde1105
Akash Kumar <akashyadav112314@gmail.com>
2022-11-13 18:11:12 +0530

    was searching for leetcode

--- Strings/reverse_word_by_word.cpp
+++ Strings/reverse_word_by_word.cpp
@@ -1,43 +1,27 @@
 #include <iostream>
 #include <string>
-using std::cout;
-using std::string;
-
-string reverse_word(string word) {
-    string rev_word = "";
-    for (int i = word.length() - 1; i >= 0; i--)
-        rev_word.append(1, word[i]);
-    rev_word.append(1, ' ');
-    return rev_word;
-}
-
-string reverse_string(string str) {
-    string reversed_string = "";
-    for (int i = str.length(); i >= 0; i--)
-        reversed_string.append(1, str[i]);
-    return reversed_string;
-}
 
 int main() {
-    string str, word = "", rev_word = "", reversed_string = "";
-    cout << "Enter a string: \n";
+    std::string str, new_str = "";
+    std::cout << "Enter a string: \n";
     getline(std::cin, str);
-    reversed_string = reverse_string(str);
     int i = 0;
-    while (i < reversed_string.length()) {
-        if (reversed_string[i] == ' ')
+    while (i < str.length()) {
+        if (str[i] == ' ')
             i++;
         else {
-            while (reversed_string[i] != ' ') {
-                word.append(1, reversed_string[i]);
+            std::string word = "";
+            while (str[i] != ' ') {
+                word += str[i];
                 i++;
-                if (i == reversed_string.length())
+                if (i == str.length())
                     break;
             }
-            rev_word = reverse_word(word);
-            cout << rev_word;
-            word = "";
-            rev_word = "";
+            std::reverse(word.begin(), word.end());
+            new_str += word + " ";
         }
     }
+    new_str = new_str.substr(0, new_str.length() - 1);
+    std::reverse(new_str.begin(), new_str.end());
+    std::cout << new_str << "\n";
 }
